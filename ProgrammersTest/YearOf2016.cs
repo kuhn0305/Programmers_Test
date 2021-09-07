fe63976c613ee0a23d098a735e41aad32545fa7d
HyunWoo Kang <kuhn0305@gmail.com>
2021-09-08 00:15:50 +0900

    Create YearOf2016.cs
    프로그래머스 코딩테스트
    [2016년]

---
+++ ProgrammersTest/YearOf2016.cs
@@ -0,0 +1,28 @@
+﻿public class YearOf2016
+{
+
+    string[] dayArr = new string[7]
+    {
+        "FRI","SAT","SUN","MON","TUE","WED","THU"
+    };
+
+    int[] monthDate = new int[12]
+    {
+      31,29,31,30,31,30,31,31,30,31,30,31
+    };
+
+    public string solution(int a, int b)
+    {
+        int dateSum = 0;
+
+        for (int index = 0; index < a - 1; index++)
+        {
+            dateSum += monthDate[index];
+        }
+
+        dateSum += b;
+
+        string answer = dayArr[--dateSum % 7];
+        return answer;
+    }
+}
