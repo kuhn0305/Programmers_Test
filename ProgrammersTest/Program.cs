d78fac6478c27dd062dc1aa0c163b109a1a5f4e3
HyunWoo Kang <kuhn0305@gmail.com>
2022-02-24 02:52:10 +0900

    Update Program.cs

--- ProgrammersTest/Program.cs
+++ ProgrammersTest/Program.cs
@@ -7,5 +7,12 @@ class Program
     {
         // Call the SolveQuestion function of the problem(Class) you want.
         // Ex) InnerProduct.SolveQuestion();
+
+        int[] truck_weights =
+        {
+            10,10,10,10,10,10,10,10,10,10
+        };
+
+        StackQueue.solution(100, 100, truck_weights);
     }
 }
