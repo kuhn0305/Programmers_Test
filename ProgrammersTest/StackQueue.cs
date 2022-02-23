4f945acd5dd3d787ae63ace703c6e029b9198b9f
HyunWoo Kang <kuhn0305@gmail.com>
2022-02-24 02:52:22 +0900

    Update StackQueue.cs
    Solve 다리를 지나는 트럭

--- ProgrammersTest/StackQueue.cs
+++ ProgrammersTest/StackQueue.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿#define 다리를지나는트럭
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,13 +11,15 @@ namespace ProgrammersTest
     class StackQueue
     {
 
+
+#if 프린터
         struct Item
         {
             public int location;
             public int priority;
         }
 
-        public static int Printer(int[] priorities, int location)
+        public static int solution(int[] priorities, int location)
         {
             int answer = 0;
 
@@ -53,5 +57,70 @@ namespace ProgrammersTest
 
             return answer;
         }
+#endif
+
+#if 다리를지나는트럭
+
+        class Truck
+        {
+            public int weight;
+            public int count;
+
+            public Truck(int w, int c)
+            {
+                weight = w;
+                count = c;
+            }
+        }
+
+        public static int solution(int bridge_length, int weight, int[] truck_weights)
+        {
+            int answer = 0;
+
+            Queue<int> truckQueue = new Queue<int>(truck_weights);
+            List<Truck> bridgeTruckList = new List<Truck>();
+            Truck currentTruck;
+
+            int totalWeight = 0;
+            int tempWeight = 0;
+
+            while(truckQueue.Count > 0 || bridgeTruckList.Count >0)
+            {
+                answer++;
+
+                for(int index = bridgeTruckList.Count - 1; index >= 0; --index)
+                {
+                    currentTruck = bridgeTruckList[index];
+                    currentTruck.count++;
+
+                    if(currentTruck.count >= bridge_length)
+                    {
+                        totalWeight -= currentTruck.weight;
+                        bridgeTruckList.RemoveAt(index);
+                    }
+                }
+
+                if(bridgeTruckList.Count < bridge_length && truckQueue.Count > 0)
+                {
+                    tempWeight = truckQueue.Peek();
+
+                    if(totalWeight + tempWeight <= weight)
+                    {
+                        totalWeight += truckQueue.Dequeue();
+
+                        Truck newTruck = new Truck(tempWeight, 0);
+
+                        bridgeTruckList.Add(newTruck);
+                    }
+                }
+            }
+
+            Console.WriteLine(answer);
+
+            return answer;
+        }
+
+#endif
+
     }
 }
