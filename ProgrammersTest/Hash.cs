7272afe844ed0cdbe3871fac0f6e8b8c2d9e2d5d
HyunWoo Kang <kuhn0305@gmail.com>
2022-02-28 02:53:23 +0900

    Solve 베스트앨범

--- ProgrammersTest/Hash.cs
+++ ProgrammersTest/Hash.cs
@@ -1,4 +1,4 @@
-﻿#define 위장
+﻿#define 베스트앨범
 
 using System;
 using System.Collections.Generic;
@@ -51,5 +51,82 @@ namespace ProgrammersTest
             return answer;
         }
 #endif
+
+#if 베스트앨범
+        class Info
+        {
+            public int plays;
+            public int number;
+
+            public Info(int p, int n)
+            {
+                plays = p;
+                number = n;
+            }
+        }
+
+        public int[] solution(string[] genres, int[] plays)
+        {
+            int[] answer;
+            Dictionary<string, List<Info>> genreDic = new Dictionary<string, List<Info>>();
+            Dictionary<string, int> genrePlayDic = new Dictionary<string, int>();
+            List<int> answerList = new List<int>();
+
+            for(int i = 0; i < genres.Length; i++)
+            {
+                Info tempInfo = new Info(plays[i], i);
+
+                if(genreDic.ContainsKey(genres[i]))
+                {
+                    genreDic[genres[i]].Add(tempInfo);
+                }
+                else
+                {
+                    List<Info> tempInfoList = new List<Info>();
+                    tempInfoList.Add(tempInfo);
+
+                    genreDic.Add(genres[i], tempInfoList);
+                }
+            }
+
+            foreach(var item in genreDic)
+            {
+                int totalPlays = 0;
+
+                foreach(Info info in item.Value)
+                    totalPlays += info.plays;
+
+                genrePlayDic.Add(item.Key, totalPlays);
+            }
+
+            var queryDesc = genrePlayDic.OrderByDescending(x => x.Value);
+
+            foreach(var item in queryDesc)
+            {
+                genreDic[item.Key].Sort((x, y) => x.number.CompareTo(y.number));
+                genreDic[item.Key].Reverse();
+                genreDic[item.Key].Sort((x,y) => x.plays.CompareTo(y.plays));
+                genreDic[item.Key].Reverse();
+
+                if(genreDic[item.Key].Count > 1)
+                {
+                    answerList.Add(genreDic[item.Key][0].number);
+                    answerList.Add(genreDic[item.Key][1].number);
+                }
+                else
+                    answerList.Add(genreDic[item.Key][0].number);
+
+            }
+
+            answer = answerList.ToArray();
+
+            foreach(int num in answer)
+                Console.WriteLine(num);
+
+            return answer;
+        }
+#endif
+
+
     }
 }
